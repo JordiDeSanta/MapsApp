6b3abefa3a7f9eeb9390e392d176481fe02e9e83
Jesus Bravo <jesusalonso.bravosilva@gmail.com>
2021-12-18 14:03:01 -0300

    Implementing funcionality buttons

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -9,6 +9,7 @@ class FullScreenMap extends StatefulWidget {
 class _FullScreenMapState extends State<FullScreenMap> {
   late MapboxMapController mapController;
   final center = LatLng(-33.51116466234621, -70.60739170732393);
+  String selectedStyle = 'mapbox://styles/jord1dev/ckxc1szha4veu14mtfkxqenkf';
   final darkStyle = 'mapbox://styles/jord1dev/ckxc1szha4veu14mtfkxqenkf';
   final lightStyle = 'mapbox://styles/jord1dev/ckxc1vken315t14pp02tkv6zk';
 
@@ -19,8 +20,43 @@ class _FullScreenMapState extends State<FullScreenMap> {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: createMap(),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            child: Icon(Icons.zoom_in),
+            onPressed: () {
+              mapController.animateCamera(CameraUpdate.zoomIn());
+            },
+          ),
+          SizedBox(height: 5),
+          FloatingActionButton(
+            child: Icon(Icons.zoom_out),
+            onPressed: () {
+              mapController.animateCamera(CameraUpdate.zoomOut());
+            },
+          ),
+          SizedBox(height: 5),
+          FloatingActionButton(
+            child: Icon(Icons.dark_mode),
+            onPressed: () {
+              if (selectedStyle == darkStyle)
+                selectedStyle = lightStyle;
+              else
+                selectedStyle = darkStyle;
+              setState(() {});
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  MapboxMap createMap() {
     return MapboxMap(
-      styleString: lightStyle,
+      styleString: selectedStyle,
       onMapCreated: _onMapCreated,
       accessToken:
           "sk.eyJ1Ijoiam9yZDFkZXYiLCJhIjoiY2t4YnpvZ2cxNGFyYTJ1cW83ZnRvN3hkOCJ9.M31CkMhz63RewDSTGPhw2Q",
